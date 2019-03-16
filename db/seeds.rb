b5ccb6fbefa031bad11b7105471c130190cf5120
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-16 19:43:18 -0400

    add solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+SchoolClass.create(title: "AA", room_number: 101)
+SchoolClass.create(title: "BB", room_number: 102)
+Student.create(first_name: "Stephen", last_name: "Yoon")
+Student.create(first_name: "Ammar", last_name: "Ali")
