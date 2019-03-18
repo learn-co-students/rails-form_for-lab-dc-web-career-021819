bbe4b9a16ff2a79e65901a236ab3948de37cd468
Sean <shonkelli@gmail.com>
2019-03-18 13:36:16 -0400

    built student controller and models

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,26 @@
 class SchoolClassesController < ApplicationController
   def new
+    @school = School.new
   end
 
   def create
+    school = School.create(school_params)
+    redirect_to school_path(school)
+  end
+
+  def show
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:title, :room_number)
   end
 end
